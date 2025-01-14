2aedb417e98368d5bacf91917163c3c005f04faf
tugcan-kartal <tugcankartal01@gmail.com>
2025-01-14 14:13:22 +0300

    fixed

--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -1,19 +1,19 @@
 import OpenAI from "openai";
 
-const openai = new OpenAI({
-  apiKey: "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",
-});
-
 export const analyze = async (prompt: string): Promise<string> => {
-  try {
-      const completion = await openai.chat.completions.create({
-          model: 'gpt-4o-mini',
-          messages: [{ role: 'user', content: prompt }],
-      });
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_KEY,
+  });
 
-      return completion.choices[0]?.message?.content || 'Cevap alınamadı.';
+  try {
+    const completion = await openai.chat.completions.create({
+      model: 'gpt-4o-mini',
+      messages: [{ role: 'user', content: prompt }],
+    });
+
+    return completion.choices[0]?.message?.content || 'Cevap alınamadı.';
   } catch (error) {
-      console.error('OpenAI Hatası:', error);
-      throw new Error('OpenAI ile bağlantı kurulamadı.');
+    console.error('OpenAI Hatası:', error);
+    throw new Error('OpenAI ile bağlantı kurulamadı.');
   }
 };
