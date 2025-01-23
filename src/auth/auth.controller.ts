401972848863c9a67927a7d5c811f6f7212fc453
tugcan-kartal <tugcankartal01@gmail.com>
2025-01-23 04:10:13 +0300

    google auth done

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get, Post, Query, UseGuards, Request, Req } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards, Request, Req, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dto/signUp.dto';
 import { LoginDto } from './dto/login.dto';
@@ -6,39 +7,36 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService){}
+  constructor(private authService: AuthService) {}
 
-    @Post('/signup')
-    signUp(@Body() signUpDto: SignUpDto): Promise<{token: string}>{
-        return this.authService.signUp(signUpDto); 
-    }
+  @Post('/signup')
+  signUp(@Body() signUpDto: SignUpDto): Promise<{ token: string }> {
+    return this.authService.signUp(signUpDto);
+  }
 
-    @Post('/login')
-    login(@Body() LoginDto: LoginDto): Promise<{token: string}>{
-        return this.authService.login(LoginDto); 
-    }
+  @Post('/login')
+  login(@Body() LoginDto: LoginDto): Promise<{ token: string }> {
+    return this.authService.login(LoginDto);
+  }
 
-    @Get("/user-details")
-    @UseGuards(AuthGuard("jwt"))
-    async userDetails(@Request() req): Promise<any>{
-        return req.user;      
-    }
+  @Get('/user-details')
+  @UseGuards(AuthGuard('jwt'))
+  async userDetails(@Request() req): Promise<any> {
+    return req.user;
+  }
 
-    @Get('/google')
-    @UseGuards(AuthGuard('google'))
-    async googleAuth() {
-        // Google ile kimlik doğrulama başlatılır
-    }
+  @Get('/google')
+  @UseGuards(AuthGuard('google'))
+  async googleAuth() {
+    // Google ile kimlik doğrulama başlatılır
+  }
 
-    @Get('/google/callback')
-    @UseGuards(AuthGuard('google'))
-    async googleAuthRedirect(@Req() req) {
-        const { user, token } = req.user; // `user` ve `token` ayrıştırılıyor
-        return {
-            message: 'Google login successful',
-            user,
-            token, // Token'ı döndür
-        };
-    }
+  @Get('/google/callback')
+  @UseGuards(AuthGuard('google'))
+  async googleAuthRedirect(@Req() req, @Res() res: Response) {
+    const { user, token } = req.user;
 
+    // Token ve kullanıcı bilgileriyle özel bir sayfaya yönlendirin
+    res.redirect(`http://localhost:3001/user/signin?token=${token}`);
+  }
 }
