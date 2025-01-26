d47fbd0117f0def2dfebd2edafedd6919f1e271e
tugcan-kartal <tugcankartal01@gmail.com>
2025-01-26 05:27:37 +0300

    updated port

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT,'0.0.0.0');
 } 
 bootstrap();
